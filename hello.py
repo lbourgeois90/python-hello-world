08ece3021be7296f3f50fe38a82e9e0b8d4dae7a
Lili Bourgeois <lbourgeois90@gmail.com>
2019-05-07 15:14:34 -0500

    basic use of variables

---
+++ hello.py
@@ -0,0 +1,25 @@
+# print Hello World
+print ("Hello, World!")
+
+# set variable myNumber to integer and print variable
+myNumber = 10
+print(myNumber)
+
+# set variable myFloater to float integer and print variable
+myFloater = 9.99
+print(myFloater)
+
+# set variable iWantToSayHello to string and print variable
+iWantToSayHello = 'Hello!'
+print(iWantToSayHello)
+
+#basic addition between two variables that are set to integer values
+one = 1
+two = 2
+three = one + two
+print(three)
+four = two + two
+print(four)
+
+#mix operators -- will throw error
+print(one + two + iWantToSayHello)
